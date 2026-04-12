74fcf237be4ab0709f8a231aa8969122ee9f1006
栗下直樹 <nao.kurishita@gmail.com>
2026-04-12 11:20:56 +0900

    追加６

--- works.js
+++ works.js
@@ -150,7 +150,7 @@ const W=[
   ["NEC",["1A46Kw3h1tWocCTFr1QONqjx4NGTFjQqu"]],
   ["SEIKO",["1VO6keFPuMoCYylSf8P4sgRuZ8HrC21Ge"]],
   ["BRUTUS",["1Z8EHm7Owc9eVkEcVQTOyjUCDUxrWrTif", "1aJ0fEUgPIcnZJaxpWLUCLtCtVvLsmMx1"]],
-  ["ヘブンバーンズレッド 1st",["1kCuFhjOwiSBcNHqr6wrnJ32Zy7U_lNv2", "1yi2NdN1Ncm6eSBtzPkqcIsL0LmtSvTLI"]],
+  ["ヘブンバーンズレッド 1st",["1kCuFhjOwiSBcNHqr6wrnJ32Zy7U_lNv2","1yi2NdN1Ncm6eSBtzPkqcIsL0LmtSvTLI","1w1aUJ5FRM_1ATVrSnImVR6xsmAmiDmSK","1AzIhCz9eyRiIGKdqME-m3EPXYzmM1zcU","1IYl2uFolgJ_qypizYHPq5Li05cXkMIkW","1YwuQUxi-NbA0D8QOFBlMrOvWxGf2EKk3"]],
   ["SAPPORO CLASSIC",["1HatEatLwFkDOke5-pZ_bIV__Id59SNVm","1PiW5tNHWsayxW7O0QubAqPrc4ytXWpFL","1SUhKb7JtWhLF2Dr2dHChdWxFw7bMiIef","1ZbAN-0SstqT4MnRNDLsGGrIRNh5RkcQf","1sN2MGlP7c0bmQjd98Ro6P5omxb5JhYLC","1fT6u8xw6JN-iqmNLK5LVYda0XXuUrZYh","1mYJqLgS9mNEqzvimj-DZQiuXUmZ8VfHA","1wwyWc4kcUn5HEdbcFqEIZp4tHb-mSdcr","1fZhAiidc8wDI8nhd54oQ9eiAbKZ4UZaJ","1G3an39MLkuKcTOfktUF29fU3uYNeTrtJ"]],
   ["N organic",["1mJW26rPGTElI1Hhmc7cEUxUcCKNWDENX","1TsZt3oPM-PCEQvvxuZUVHKMP7QYbbOrv","18O-rjhVF7Lta9bK-QP5AcQhHDctPIaMc","1js38PaGSgoB695WBeLcn0cErDQfmOQV3","1OE6n6IB70EO2xT0G4_XMeME_pktjpVJ_","1bTYwORi-t4dyHVt2fnZ7AEBHd5wpEbBl","1XDfOxUZiuzDmik0hNNqEMpw7OF4RBaNB"]],
   ["バイトル 乃木坂",["1Ny6Mdv9_rECNipPTWG6tQ8qUr5Hcj4W3","1C-ES2UxAxffGSNFUiTzwU2Qruas9FCn5","1oNYGAh3xSMXkCg3JwFqyzRl-B9g618ul","1a-uWcOj3gQXCS-5EN1ew80dNPHGQaMBF"]],
